09ba710ab89cd2724902aa26da8b02f1b2a35a8b
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-03-23 12:22:10 +0300

    list initial messages frombackend

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,26 +1,28 @@
-import React, { useEffect } from 'react'
-import ChatForm from './ChatForm'
-import ChatList from './ChatList'
-import {init, subscribeChat} from "../socketApi"
-import {useChat} from '../context/ChatContext'
+import React, { useEffect } from "react";
+import ChatForm from "./ChatForm";
+import ChatList from "./ChatList";
+import { init, subscribeChat, subscribeInitialMessages } from "../socketApi";
+import { useChat } from "../context/ChatContext";
 // import styles from "./styles.module.css";
 
-
 function Container() {
-  const {setMessages} = useChat();
+  const { setMessages } = useChat();
+  
   useEffect(() => {
-    init()
+    init();
+
+    subscribeInitialMessages((messages) => setMessages(messages));
 
     subscribeChat((message) => {
-      setMessages(prevState => [...prevState, {message}])
-    })
-  })
+      setMessages((prevState) => [...prevState, { message }]);
+    });
+  }, []);
   return (
     <div className="App">
-        <ChatList />
-        <ChatForm />
+      <ChatList />
+      <ChatForm />
     </div>
-  )
+  );
 }
 
-export default Container
+export default Container;
--- client/src/socketApi.js
+++ client/src/socketApi.js
@@ -8,18 +8,26 @@ export const init = () => {
     transports: ["websocket"],
   });
 
-  socket.on("connect", () => console.log("Connected!"))
+  socket.on("connect", () => console.log("Connected!"));
 };
 
-
 export const sendMessage = (message) => {
-    if(socket) socket.emit("new-message", message)
-}
+  if (socket) socket.emit("new-message", message);
+};
 export const subscribeChat = (cb) => {
-    if(!socket) return;
+  if (!socket) return;
 
-    socket.on("receive-message", (message) => {
-        console.log("Yeni mesaj var:", message);
-        cb(message)
-    })
-}
+  socket.on("receive-message", (message) => {
+    console.log("Yeni mesaj var:", message);
+    cb(message);
+  });
+};
+
+export const subscribeInitialMessages = (cb) => {
+  if (!socket) return;
+
+  socket.on("message-list", (messages) => {
+    console.log("Initial:", messages);
+    cb(messages);
+  });
+};
